f9faa52d662a8b33b757362c8bcf744164f6631f
Thanh Nguyen <mlthanh@gmail.com>
2023-07-24 17:18:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,6 @@
 // create web server
-// create a server object:
-http.createServer(function (req, res) {
-    // res.write('Hello World!'); //write a response to the client
-    // res.end(); //end the response
-    // res.end('<h1>Hello World!</h1>');
-    // res.end('<h1>Hello World!</h1><p>Hello World!</p>');
-    // res.end('<h1>Hello World!</h1><p>Hello World!</p><p>Hello World!</p>');
-    // res.end('<h1>Hello World!</h1><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p>');
-    // res.end('<h1>Hello World!</h1><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p>');
-    // res.end('<h1>Hello World!</h1><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p>');
-    // res.end('<h1>Hello World!</h1><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p><p>Hello World!</p>');
-}).listen(8080); //the server object listens on port 8080
+const http = require('http');
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey ninjas');
+});
